bd2841301ea27f519a70cf480247ef4ee4f89515
nin <ninthegoob@gmail.com>
2025-10-12 17:07:42 +0200

    add colors, errors, copying and renaming

--- funstuff.py
+++ funstuff.py
@@ -1,7 +1,9 @@
 # where the magic happens
 
 import time
+from colorama import Style
 
 def die():
     time.sleep(4)
+    print(Style.RESET_ALL)
     quit()
--- main.py
+++ main.py
@@ -2,25 +2,38 @@ import os
 import zipfile
 import shutil
 import funstuff as f
+from colorama import Fore
 
-scrunchfile = input("choose the path of the .sb3 file you want to convert: ")
-if not scrunchfile.endswith(".sb3"):
-    print("error: not an sb3 file!!")
+inputpath = input("write the path of the .sb3 file you want to convert (dont forget to double the slashes!): ")
+if not inputpath.endswith(".sb3"):
+    print(Fore.RED + "error: not an sb3 file!!")
     f.die()
-elif not os.path.exists(scrunchfile):
-    print("error: not a valid path or file!!")
+elif not os.path.exists(inputpath):
+    print(Fore.RED + "error: not a valid path or file!!")
     f.die()
+filename1 = os.path.basename(inputpath)
+filename2 = filename1.replace(".sb3", "")
 
-output = input("enter output path: ")
+output = input("write the output path: ")
 if not os.path.exists(output):
-    print("error: not a valid path or file!!")
+    print(Fore.RED + "error: not a valid path or file!!")
     f.die()
 
 title = input("insert new title: ")
+if title == (""):
+    print(Fore.RED + "error: not a valid folder name!!")
+    f.die()
+elif os.path.exists(output + "/"+ title):
+    print(Fore.RED + "error: path already exists!!")
+    f.die()
 
-input("weeeee")
 os.chdir(output)
+print("Creating folder...")
 os.mkdir(title)
-# COPY SB3 IN DIR
-# RENAME IT
+print("done!\ncopying file...")
+shutil.copy(inputpath, output + "/" + title)
+print("done!\nrenaming file...")
+os.chdir(output + "/" + title)
+os.rename(filename1, filename2 + ".zip")
+print("done!\nextracting file...")
 # EXTRACT IT
